b02eb3e5924ae96b2a966f5e2d6a1f6b16ccc443
Arseniy Sidorov <arseniy.sidorov2013@gmail.com>
2022-02-03 20:49:42 +0500

    added banner component

---
+++ src/Banner/Banner.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+import QRImage from '../res/qr.svg';
+import './Banner.css';
+
+interface BannerProps {
+    btnOnClick: (popup: boolean) => void; 
+}
+
+const Banner: React.FC<BannerProps> = ({ btnOnClick }) => {
+    const [hidden, setHidden] = useState(true);
+    const btnRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+        btnRef.current?.focus();
+        setTimeout(() => {
+            setHidden(false);
+        }, 1000);
+    }, []);
+
+    return (
+        <div className={`banner ${hidden ? 'hidden' : ''}`}>
+            <span className="primary-text">ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША!<br />ПОДАРИТЕ ЕМУ СОБАКУ!</span>
+            <img className="qr-image" src={QRImage} alt="QR" />
+            <span className="secondary-text">Сканируйте QR-код или нажмите ОК</span>
+            <button ref={btnRef} onClick={() => btnOnClick(true)} className="ok-button">OK</button>
+        </div>
+    );
+};
+export default Banner;
